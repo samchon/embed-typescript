9383c8a0f40b962bad431493e97c2f5d67872935
Jeongho Nam <samchon.github@gmail.com>
2025-05-17 22:54:57 +0900

    Fix await import statement

--- test/src/features/test_compile_failure.ts
+++ test/src/features/test_compile_failure.ts
@@ -8,10 +8,7 @@ import typiaTransform from "typia/lib/transform";
 
 export async function test_compile_failure(): Promise<void> {
   const compiler: EmbedTypeScript = new EmbedTypeScript({
-    external: (await import("../external.json")).default as Record<
-      string,
-      string
-    >,
+    external: await import("../external.json" as any),
     compilerOptions: {
       target: ts.ScriptTarget.ES2015,
       module: ts.ModuleKind.CommonJS,
