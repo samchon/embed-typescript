f05785ea7537a3e3ea302ad18b894234050a54c2
Jeongho Nam <samchon.github@gmail.com>
2025-08-25 14:54:33 +0900

    Export `IEmbedTypeScriptFountain` type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./EmbedTypeScript";
 export * from "./IEmbedTypeScriptDiagnostic";
+export * from "./IEmbedTypeScriptFountain";
 export * from "./IEmbedTypeScriptProps";
 export * from "./IEmbedTypeScriptResult";
 export * from "./IEmbedTypeScriptTransformation";
